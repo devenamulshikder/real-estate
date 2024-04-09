9bc32a9b445b9758cf74f115f04ef89452e4a085
devenamulshikder <devenamulshikder@gmail.com>
2024-04-09 11:04:09 +0600

    fake data added

---
+++ src/components/OurService.jsx
@@ -0,0 +1,76 @@
+import { FaUsers } from "react-icons/fa";
+import { IoCloudUploadOutline, IoHome } from "react-icons/io5";
+import { SlLocationPin } from "react-icons/sl";
+const OurService = () => {
+  return (
+    <div>
+      <h1 className="text-xl md:text-2xl my-4 md:my-8 text-center lg:text-4xl lg:my-16">
+        Our Services
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-5 lg:gap-10">
+        <div className="flex flex-col space-y-4 text-center justify-center items-center hover:bg-green-100 lg:p-4 rounded-md hover:scale-110">
+          <div className="text-5xl text-green-600">
+            <SlLocationPin />
+          </div>
+          <div>
+          <h1 className=" md:text-lg lg:text-xl font-semibold">Find places anywhere in the world</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
+            veritatis.
+          </p>
+          </div>
+        </div>
+        <div>
+        <div className="flex flex-col space-y-4 text-center justify-center items-center hover:bg-green-100 lg:p-4 rounded-md hover:scale-110">
+          <div className="text-5xl text-green-600">
+          <FaUsers />
+          </div>
+          <div>
+          <h1 className=" md:text-lg lg:text-xl font-semibold">Find places anywhere in the world</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
+            veritatis.
+          </p>
+          </div>
+        </div>
+
+        </div>
+        <div>
+        <div className="flex flex-col space-y-4 text-center justify-center items-center hover:bg-green-100 lg:p-4 rounded-md hover:scale-110">
+          <div className="text-5xl text-green-600">
+          <IoHome/>
+
+          </div>
+          <div>
+          <h1 className=" md:text-lg lg:text-xl font-semibold">Find places anywhere in the world</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
+            veritatis.
+          </p>
+          </div>
+        </div>
+        </div>
+        <div>
+        <div className="flex flex-col space-y-4 text-center justify-center items-center hover:bg-green-100 lg:p-4 rounded-md hover:scale-110">
+          <div className="text-5xl text-green-600">
+          <IoCloudUploadOutline />
+
+          </div>
+          <div>
+          <h1 className=" md:text-lg lg:text-xl font-semibold">Find places anywhere in the world</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
+            veritatis.
+          </p>
+          </div>
+        </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default OurService;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 
 import Banner from "../components/Banner";
+import OurService from "../components/OurService";
 
 const Home = () => {
     return (
         <div className="">
             <Banner/>
+            <OurService/>
         </div>
     );
 };
