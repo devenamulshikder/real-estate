ef1d2a5512fd6df9d9f9cf8efa30b9fedddfceea
devenamulshikder <devenamulshikder@gmail.com>
2024-04-09 19:00:42 +0600

    footer added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,15 +1,16 @@
-
 import { Outlet } from "react-router-dom";
 import Nav from "../pages/shared/Nav";
+import Footer from "../components/Footer";
 const Root = () => {
-    return (
-        <div>
-            <div className="max-w-7xl mx-auto">
-            <Nav/>
-            <Outlet></Outlet>
-        </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto">
+        <Nav />
+        <Outlet></Outlet>
+      </div>
+      <Footer/>
+    </div>
+  );
 };
 
 export default Root;
