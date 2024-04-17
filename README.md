0442d659f6e867167a69f0761b6982c716b36644
devenamulshikder <devenamulshikder@gmail.com>
2024-04-17 21:09:53 +0600

    projects replaces done

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Dream Sites
-# Live Link: 
+
+# Live Link:
+
 - [Dream-Sites](https://assignment-9-real-estate-c65a0.web.app/)
 
-# Facilities of the Websites: 
+# Facilities of the Websites:
+
 - Smooth Animation Experience: Elevate user interaction with smooth animations throughout the website, enhancing the overall browsing experience and providing a visually appealing interface.
 
 - User Authentication: Enable users to securely log in and log out of their accounts, ensuring their personal information and preferences are protected while accessing exclusive features and content.
 
 - Profile Management:Empower users to update their profiles with ease, allowing them to edit personal information, manage preferences, and customize their experience on the platform according to their needs.
 
-- Social Media Integration: Simplify the login process by integrating social media authentication options, enabling users to log in  seamlessly using their existing social media accounts, increasing accessibility and convenience.
+- Social Media Integration: Simplify the login process by integrating social media authentication options, enabling users to log in seamlessly using their existing social media accounts, increasing accessibility and convenience.
 - Easily Contact: User can easily contact with us and we support them very fast.
-
 
 # Resources & Technologies Used From:
 
@@ -26,4 +28,3 @@
 - [Firebase-Auth](https://firebase.com/)
 - [Google-font](https://fonts.google.com/)
 - [React-Hook-Form](https://react-hook-form.com/)
-
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,9 +20,6 @@ const Login = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-
-
-
   const handleLogin = (e) => {
     e.preventDefault();
     setError(null);
@@ -34,10 +31,10 @@ const Login = () => {
         toast("Login Successfully!");
         navigate(location?.state ? location.state : "/");
       })
-      .catch(error=>{
-        toast(`${error.message}Please Register first..!`)
-        navigate('/register')
-      })
+      .catch((error) => {
+        toast(`${error.message}Please Register first..!`);
+        navigate("/register");
+      });
   };
 
   const handleGoogleLonIn = () => {
@@ -63,109 +60,109 @@ const Login = () => {
   return (
     <div>
       <div
-      data-aos="fade-down"
-      data-aos-duration="1200"
-      className="w-full lg:mt-32 mx-auto lg:w-[1000px] shadow-lg bg-white flex group text-gray-600"
-    >
-      <Helmet>
-        <title>Dream sites || Login</title>
-      </Helmet>
-      <div className="w-1/2 min-h-full bg-[#38b469] relative overflow-hidden hidden lg:block">
-        <h1 className="text-white text-2xl absolute bottom-3 right-3 text-right">
-          Hey! <br /> Please
-          <br /> Login Our Sites
-        </h1>
-        <span className="bg-green-700/20 w-32 h-32 -top-8 -left-8 rounded-full absolute z-20 group-hover:w-56 group-hover:h-56 duration-500"></span>
-        <span className="bg-green-700/50 w-36 h-36 -top-5 -left-5  rounded-full absolute z-10"></span>
-      </div>
-      <form onSubmit={handleLogin} className="p-8 flex-1">
-        <h1 className="text-4xl pb-4">Login</h1>
-        <div className="space-y-5">
+        data-aos="fade-down"
+        data-aos-duration="1200"
+        className="w-full lg:mt-32 mx-auto lg:w-[1000px] shadow-lg bg-white flex group text-gray-600"
+      >
+        <Helmet>
+          <title>Dream sites || Login</title>
+        </Helmet>
+        <div className="w-1/2 min-h-full bg-[#38b469] relative overflow-hidden hidden lg:block">
+          <h1 className="text-white text-2xl absolute bottom-3 right-3 text-right">
+            Hey! <br /> Please
+            <br /> Login Our Sites
+          </h1>
+          <span className="bg-green-700/20 w-32 h-32 -top-8 -left-8 rounded-full absolute z-20 group-hover:w-56 group-hover:h-56 duration-500"></span>
+          <span className="bg-green-700/50 w-36 h-36 -top-5 -left-5  rounded-full absolute z-10"></span>
+        </div>
+        <form onSubmit={handleLogin} className="p-8 flex-1">
+          <h1 className="text-4xl pb-4">Login</h1>
+          <div className="space-y-5">
+            <div>
+              <label htmlFor="email_" className="block">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="example@example.com"
+                className="p-3 block w-full shadow-lg outline-none border-2 rounded-md border-dashed  invalid:border-red-700 valid:border-[#38b469]"
+              />
+            </div>
+            <div className="relative ">
+              <label htmlFor="password_" className="block">
+                Password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder=".............."
+                required
+                min={5}
+                className="p-3 block w-full shadow-lg outline-none border-2 rounded-md border-dashed invalid:border-red-700 valid:border-[#38b469]"
+              />
+              <span
+                className=" absolute right-3 bottom-4"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+              </span>
+            </div>
+          </div>
+          <div className="flex justify-end text-xs ">
+            <a href="#" className="hover:underline">
+              Forgot Password?
+            </a>
+          </div>
           <div>
-            <label htmlFor="email_" className="block">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              required
-              placeholder="example@example.com"
-              className="p-3 block w-full shadow-lg outline-none border-2 rounded-md border-dashed  invalid:border-red-700 valid:border-[#38b469]"
-            />
+            {error && (
+              <h5 className="text-red-400">
+                Please don't try to login with wrong Password..!
+              </h5>
+            )}
           </div>
-          <div className="relative ">
-            <label htmlFor="password_" className="block">
-              Password
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              placeholder=".............."
-              required
-              min={5}
-              className="p-3 block w-full shadow-lg outline-none border-2 rounded-md border-dashed invalid:border-red-700 valid:border-[#38b469]"
-            />
-            <span
-              className=" absolute right-3 bottom-4"
-              onClick={() => setShowPassword(!showPassword)}
+
+          <div>
+            <button className="btn btn-outline my-4 border-2 border-dashed border-[#38b469] hover:bg-[#38b469]">
+              Log in
+            </button>
+          </div>
+
+          <div className="flex items-center pt-4 space-x-2 mb-2">
+            <div className="flex-1 h-px bg-gray-300"></div>
+            <p className="text-sm text-gray-600">Login with social accounts</p>
+            <div className="flex-1 h-px bg-gray-300"></div>
+          </div>
+
+          <div className="flex justify-center space-x-4 mb-2">
+            <button
+              onClick={handleGoogleLonIn}
+              aria-label="Log in with Google"
+              className="p-3 rounded-full hover:bg-gray-200"
             >
-              {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
-            </span>
+              <FaGoogle size={30} />
+            </button>
+            <button
+              onClick={handleGithubLonIn}
+              aria-label="Log in with Twitter"
+              className="p-3 rounded-full hover:bg-gray-200"
+            >
+              <FaGithub size={30} />
+            </button>
           </div>
-        </div>
-        <div className="flex justify-end text-xs ">
-          <a href="#" className="hover:underline">
-            Forgot Password?
-          </a>
-        </div>
-        <div>
-          {error && (
-            <h5 className="text-red-400">
-              Please don't try to login with wrong Password..!
-            </h5>
-          )}
-        </div>
 
-        <div>
-          <button className="btn btn-outline my-4 border-2 border-dashed border-[#38b469] hover:bg-[#38b469]">
-            Log in
-          </button>
-        </div>
-
-        <div className="flex items-center pt-4 space-x-2 mb-2">
-          <div className="flex-1 h-px bg-gray-300"></div>
-          <p className="text-sm text-gray-600">Login with social accounts</p>
-          <div className="flex-1 h-px bg-gray-300"></div>
-        </div>
-
-        <div className="flex justify-center space-x-4 mb-2">
-          <button
-            onClick={handleGoogleLonIn}
-            aria-label="Log in with Google"
-            className="p-3 rounded-full hover:bg-gray-200"
-          >
-            <FaGoogle size={30} />
-          </button>
-          <button
-            onClick={handleGithubLonIn}
-            aria-label="Log in with Twitter"
-            className="p-3 rounded-full hover:bg-gray-200"
-          >
-            <FaGithub size={30} />
-          </button>
-        </div>
-
-        <p className="">
-          Do not have an Accouont?{" "}
-          <Link
-            className="underline font-semibold hover:text-[#38b469]"
-            to="/register"
-          >
-            Register!
-          </Link>
-        </p>
-      </form>
-    </div>
+          <p className="">
+            Do not have an Accouont?{" "}
+            <Link
+              className="underline font-semibold hover:text-[#38b469]"
+              to="/register"
+            >
+              Register!
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
