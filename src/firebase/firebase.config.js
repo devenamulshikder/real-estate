4c7e51679829a4a065b807c6a50613b854a30dfb
devenamulshikder <devenamulshikder@gmail.com>
2024-09-28 23:53:06 +0600

    firebase config change

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,8 +2,8 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  apiKey: import.meta.env.VITE_APIKEY,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
